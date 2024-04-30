fe2b23b2c0024f64c7efeb065be25bdf38da8cfe
Warda Rasool <89411637+77warda@users.noreply.github.com>
2024-04-30 20:06:51 +0500

    latest code

--- src/app/live-kit-room/live-kit-room.component.ts
+++ src/app/live-kit-room/live-kit-room.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Participant, RemoteTrack, Room, RoomEvent } from 'livekit-client';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  Participant,
+  RemoteParticipant,
+  RemoteTrack,
+  RemoteTrackPublication,
+  Room,
+  RoomEvent,
+} from 'livekit-client';
 import { LiveKitService } from '../livekit.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
@@ -8,10 +15,78 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./live-kit-room.component.scss'],
 })
 export class LiveKitRoomComponent {
+  sharedLayout!: boolean;
+  withVideo!: boolean;
+  participants = [
+    {
+      name: 'GJ',
+      dp: 'https://img.freepik.com/free-photo/beautiful-shot-tree-savanna-plains-with-blue-sky_181624-22049.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'George John',
+    },
+    {
+      name: 'GJ',
+      title: 'John Doe',
+    },
+    {
+      name: 'GJ',
+      dp: 'https://img.freepik.com/premium-photo/landscape-africa-with-river-trees-mountains-horizon-cloudy-sky-3d-illustration_76964-3101.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'Henry Peel',
+    },
+    {
+      name: 'GJ',
+      title: 'New one',
+    },
+    {
+      name: 'GJ',
+      dp: 'https://img.freepik.com/premium-photo/landscape-africa-with-river-trees-mountains-horizon-cloudy-sky-3d-illustration_76964-3101.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'Henry Peel',
+    },
+    {
+      name: 'GJ',
+      dp: 'https://img.freepik.com/premium-photo/beautiful-tropical-beach-island_213396-4088.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'New one',
+    },
+    {
+      name: 'GJ',
+      title: 'Henry Peel',
+    },
+  ];
+  images = [
+    {
+      src: 'https://img.freepik.com/free-photo/beautiful-shot-tree-savanna-plains-with-blue-sky_181624-22049.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'George John',
+    },
+    {
+      src: 'https://img.freepik.com/free-photo/beautiful-shot-snowy-mountains-with-dark-blue-sky_181624-2640.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'John Doe',
+    },
+    {
+      src: 'https://img.freepik.com/premium-photo/landscape-africa-with-river-trees-mountains-horizon-cloudy-sky-3d-illustration_76964-3101.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'Henry Peel',
+    },
+    {
+      src: 'https://img.freepik.com/premium-photo/beautiful-tropical-beach-island_213396-4088.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'New one',
+    },
+    {
+      src: 'https://img.freepik.com/premium-photo/landscape-africa-with-river-trees-mountains-horizon-cloudy-sky-3d-illustration_76964-3101.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'Henry Peel',
+    },
+    {
+      src: 'https://img.freepik.com/premium-photo/beautiful-tropical-beach-island_213396-4088.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'New one',
+    },
+    {
+      src: 'https://img.freepik.com/premium-photo/landscape-africa-with-river-trees-mountains-horizon-cloudy-sky-3d-illustration_76964-3101.jpg?size=626&ext=jpg&ga=GA1.1.490661761.1705061609&semt=sph',
+      title: 'Henry Peel',
+    },
+  ];
+
   roomDetails: { wsURL: string; token: string } | null = null;
   nameForm!: FormGroup;
   isMeetingStarted = false;
   stream: MediaStream | undefined;
+  participantNameDisconnected: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -22,32 +97,77 @@ export class LiveKitRoomComponent {
     });
   }
   ngOnInit() {
-    this.livekitService.audioVideoHandler();
+    this.livekitService.remoteVideoTrackSubscribed.subscribe(
+      (
+        track: RemoteTrack,
+        publication: RemoteTrackPublication,
+        participant: RemoteParticipant
+      ) => {
+        this.livekitService.handleTrackSubscribed(
+          track,
+          publication,
+          participant
+        );
+      }
+    );
+    this.livekitService.remoteAudioTrackSubscribed.subscribe(
+      (
+        track: RemoteTrack,
+        publication: RemoteTrackPublication,
+        participant: RemoteParticipant
+      ) => {
+        this.livekitService.handleTrackSubscribed(
+          track,
+          publication,
+          participant
+        );
+      }
+    );
   }
   ngAfterViewInit(): void {
-    this.livekitService.remoteVideoTrackSubscribed.subscribe((track) => {
-      this.displayRemoteVideo(track);
-    });
+    // this.livekitService.remoteVideoTrackSubscribed.subscribe((track) => {
+    //   this.displayRemoteVideo(track);
+    // });
+    // this.livekitService.remoteAudioTrackSubscribed.subscribe((track) => {
+    //   // Handle remote audio track
+    //   console.log('Received remote audio track:', track);
+    //   this.displayRemoteAudio(track);
+    //   // You can implement further logic here, such as playing the audio track
+    // });
   }
 
-  displayRemoteVideo(track: RemoteTrack): void {
-    const container = document.getElementById('remoteVideoContainer');
-    if (container) {
-      const element = document.createElement('video');
-      element.srcObject = new MediaStream([track.mediaStreamTrack]);
-      element.autoplay = true;
-      element.controls = false;
-      container.appendChild(element);
-    } else {
-      console.error('Remote video container not found');
-    }
-  }
+  // displayRemoteVideo(track: RemoteTrack): void {
+  //   const container = document.getElementById('remoteVideoContainer');
+  //   if (container) {
+  //     const element = document.createElement('video');
+  //     element.srcObject = new MediaStream([track.mediaStreamTrack]);
+  //     element.autoplay = true;
+  //     element.controls = false;
+  //     container.appendChild(element);
+  //   } else {
+  //     console.error('Remote video container not found');
+  //   }
+  // }
+  // private displayRemoteAudio(track: RemoteTrack): void {
+  //   const audioElement = document.createElement('audio');
+  //   audioElement.srcObject = new MediaStream([track.mediaStreamTrack]);
+  //   audioElement.autoplay = true;
+  //   audioElement.controls = false;
+
+  //   const container = document.getElementById('remoteAudioContainer');
+  //   if (container) {
+  //     container.appendChild(audioElement);
+  //   } else {
+  //     console.error('Remote audio container not found');
+  //   }
+  // }
+
   async startMeeting() {
     const name = this.nameForm.value.name;
     console.log('Entered name:', name);
     const wsURL = 'wss://vc-ua59wquz.livekit.cloud';
     const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6IkFydGlzdCBSb29tIiwiY2FuUHVibGlzaCI6dHJ1ZSwiY2FuU3Vic2NyaWJlIjp0cnVlfSwiaWF0IjoxNzE0MTEwNTk3LCJuYmYiOjE3MTQxMTA1OTcsImV4cCI6MTcxNDEzMjE5NywiaXNzIjoiQVBJVVdiRUs3Qmd2NHVrIiwic3ViIjoiV2FyZGEiLCJqdGkiOiJXYXJkYSJ9.FSEJ4G-LGYTo5zAYSYb4GPoqkjA2yOpKTgDNVBNx-WA';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6ImN1cnJlbnQiLCJjYW5QdWJsaXNoIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWV9LCJpYXQiOjE3MTQ0ODE4MDAsIm5iZiI6MTcxNDQ4MTgwMCwiZXhwIjoxNzE0NTAzNDAwLCJpc3MiOiJBUElVV2JFSzdCZ3Y0dWsiLCJzdWIiOiJBaHNhbiIsImp0aSI6IkFoc2FuIn0.gpU8bBWyreCwUQrJe4YhK2mciL3I7F1Aeo7Z8_XvtSE';
     try {
       await this.livekitService.connectToRoom(wsURL, token);
       await this.livekitService.enableCameraAndMicrophone();
@@ -70,18 +190,40 @@ export class LiveKitRoomComponent {
   recordingTime = '00:22:23';
   isScreenSharing = false;
 
-  toggleScreenShare() {
+  // toggleScreenShare() {
+  //   this.isScreenSharing = !this.isScreenSharing;
+  //   if (this.isScreenSharing) {
+  //     this.iconColor = 'green';
+  //     this.livekitService.startScreenSharing();
+  //   } else {
+  //     this.iconColor = 'black';
+  //   }
+  // }
+  async toggleScreenShare() {
     this.isScreenSharing = !this.isScreenSharing;
     if (this.isScreenSharing) {
       this.iconColor = 'green';
     } else {
       this.iconColor = 'black';
     }
+    try {
+      await this.livekitService.toggleScreenShare();
+      // await this.livekitService.participantScreenShare();
+    } catch (error) {
+      console.error('Error toggling sharescreen:', error);
+      // Handle error (e.g., show an error message to the user)
+    }
   }
+
   async toggleVideo() {
-    await this.livekitService.toggleVideo();
-    this.isVideoOn = !this.isVideoOn;
-    this.stream = await this.livekitService.startCamera();
+    try {
+      await this.livekitService.toggleVideo();
+      const localParticipant = this.livekitService.room.localParticipant;
+      this.isVideoOn = localParticipant.isCameraEnabled;
+      this.stream = await this.livekitService.startCamera();
+    } catch (error) {
+      console.error('Error toggling video:', error);
+    }
   }
 
   // toggleMic() {
--- src/app/livekit.service.ts
+++ src/app/livekit.service.ts
@@ -3,19 +3,26 @@ import { HttpClient } from '@angular/common/http';
 import {
   LocalParticipant,
   LocalTrack,
+  LocalTrackPublication,
   RemoteParticipant,
   RemoteTrack,
   RemoteTrackPublication,
   Room,
   RoomEvent,
+  Track,
+  VideoQuality,
 } from 'livekit-client';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LiveKitService {
-  private room!: Room;
+  room!: Room;
   remoteVideoTrackSubscribed = new EventEmitter<RemoteTrack>();
+  remoteParticipantName: string = '';
+  participantDisconnected = new EventEmitter<string>();
+  remoteAudioTrackSubscribed = new EventEmitter();
+  private isScreenSharingEnabled = false;
   constructor() {}
 
   async connectToRoom(wsURL: string, token: string): Promise<void> {
@@ -29,9 +36,35 @@ export class LiveKitService {
       RoomEvent.TrackSubscribed,
       this.handleTrackSubscribed.bind(this)
     );
+    this.room.on(
+      RoomEvent.ParticipantDisconnected,
+      this.handleParticipantDisconnected.bind(this)
+    );
   }
 
-  private handleTrackSubscribed(
+  private handleParticipantDisconnected(participant: RemoteParticipant) {
+    console.log('Participant disconnected:', participant);
+    this.participantDisconnected.emit(participant.identity);
+    // Remove video element associated with the disconnected participant
+    this.removeVideoElement(participant);
+  }
+
+  private removeVideoElement(participant: RemoteParticipant) {
+    const container = document.getElementById('remoteVideoContainer');
+    if (container) {
+      // Find and remove video element for the disconnected participant
+      const videoElements = container.getElementsByTagName('video');
+      for (let i = 0; i < videoElements.length; i++) {
+        const video = videoElements[i];
+        if (video.srcObject) {
+          video.remove();
+          break;
+        }
+      }
+    }
+  }
+
+  handleTrackSubscribed(
     track: RemoteTrack,
     publication: RemoteTrackPublication,
     participant: RemoteParticipant
@@ -41,9 +74,25 @@ export class LiveKitService {
     console.log('Participant:', participant);
 
     if (track.kind === 'video') {
-      this.remoteVideoTrackSubscribed.emit(track);
+      const container = document.getElementById('remoteVideoContainer');
+      if (container) {
+        const element = track.attach();
+        container.appendChild(element);
+        this.remoteParticipantName = participant.identity; // Associate video element with participant
+      } else {
+        console.error('Remote video container not found');
+      }
+    } else if (track.kind === 'audio') {
+      const container = document.getElementById('remoteAudioContainer');
+      if (container) {
+        const element = track.attach();
+        container.appendChild(element);
+      } else {
+        console.error('Remote audio container not found');
+      }
     }
   }
+
   async enableCameraAndMicrophone(): Promise<void> {
     if (!this.room) {
       throw new Error('Room not Enabled.');
@@ -79,15 +128,86 @@ export class LiveKitService {
       return undefined;
     }
   }
-  // async stopCamera(): Promise<MediaStream | undefined> {
-  //   try {
-  //     const stream = await navigator.mediaDevices.getUserMedia({
-  //       video: false,
-  //     });
-  //     return stream;
-  //   } catch (error) {
-  //     console.error('Error accessing the camera:', error);
-  //     return undefined;
+
+  // async toggleScreenShare(): Promise<void> {
+  //   if (this.isScreenSharingEnabled) {
+  //     this.room.localParticipant.setScreenShareEnabled(false);
+  //     this.isScreenSharingEnabled = false;
+  //   } else {
+  //     this.room.localParticipant.setScreenShareEnabled(true);
+  //     this.isScreenSharingEnabled = true;
   //   }
   // }
+  // ===================
+  // async participantScreenShare(): Promise<void> {
+  //   if (!this.isScreenSharingEnabled) {
+  //     try {
+  //       const stream = await navigator.mediaDevices.getDisplayMedia({
+  //         video: true,
+  //       });
+  //       const container = document.getElementById('localScreenShareContainer');
+  //       if (container) {
+  //         const videoElement = document.createElement('video');
+  //         videoElement.srcObject = stream;
+  //         videoElement.autoplay = true;
+  //         container.appendChild(videoElement);
+  //         this.isScreenSharingEnabled = true;
+  //       } else {
+  //         console.error('Local screen share container not found');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error starting screen sharing:', error);
+  //       // Handle error (e.g., show an error message to the user)
+  //     }
+  //   } else {
+  //     this.room.localParticipant.setScreenShareEnabled(true);
+  //     this.isScreenSharingEnabled = true;
+  //     const container = document.getElementById('localScreenShareContainer');
+  //     if (container) {
+  //       container.innerHTML = ''; // Remove the video element from the container
+  //       this.isScreenSharingEnabled = false;
+  //     } else {
+  //       console.error('Local screen share container not found');
+  //     }
+  //   }
+  // }
+  async toggleScreenShare(): Promise<void> {
+    // Object.values(this.room.localParticipant.videoTrackPublications).forEach((videoTrack: LocalTrackPublication) => videoTrack.track?.mediaStream)
+
+    try {
+      if (!this.isScreenSharingEnabled) {
+        const stream = await navigator.mediaDevices.getDisplayMedia({
+          video: true,
+        });
+        const container = document.getElementById('localScreenShareContainer');
+        if (container) {
+          const videoElement = document.createElement('video');
+          videoElement.srcObject = stream;
+          videoElement.autoplay = true;
+          container.appendChild(videoElement);
+          this.isScreenSharingEnabled = true;
+
+          // Publish the screen sharing stream to the room
+          await this.room.localParticipant.publishTrack(
+            stream.getVideoTracks()[0],
+            { name: 'screen_share' }
+          );
+        } else {
+          console.error('Local screen share container not found');
+        }
+      } else {
+        // this.room.localParticipant.unpublishTrack('screen_share'); // Unpublish the screen sharing track
+        this.isScreenSharingEnabled = false;
+        const container = document.getElementById('localScreenShareContainer');
+        if (container) {
+          container.innerHTML = ''; // Remove the video element from the container
+        } else {
+          console.error('Local screen share container not found');
+        }
+      }
+    } catch (error) {
+      console.error('Error toggling screen sharing:', error);
+      // Handle error (e.g., show an error message to the user)
+    }
+  }
 }
